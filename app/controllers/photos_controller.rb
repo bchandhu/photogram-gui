7f740c74cf0f9df9175e25ed62a894d22927605d
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-03-22 17:58:53 +0000

    added a route

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,11 @@
 class PhotosController < ApplicationController
-  def index
-    @list_of_photos = Photo.all.order({ :created_at => :desc })
+  def 
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
 
     render({ :template => "photo_templates/index"} )
   end
-
+  def show
+    
+  end
 end
