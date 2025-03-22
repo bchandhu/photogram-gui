a5bad861fd5b3375eb2b5ece135dc0892fe9fc33
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-03-22 18:00:14 +0000

    corrected the action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,11 @@
 class PhotosController < ApplicationController
-  def 
+  def index
     matching_photos = Photo.all
     @list_of_photos = matching_photos.order({ :created_at => :desc })
 
     render({ :template => "photo_templates/index"} )
   end
   def show
-    
+
   end
 end
